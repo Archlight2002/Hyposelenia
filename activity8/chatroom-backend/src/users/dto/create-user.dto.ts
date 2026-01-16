7e4bf17478f8ea82f041b285cf5a923ba1e27cd1
KeaneYap <ic.matthewkeane.mariano@cvsu.edu.ph>
2026-01-17 01:10:42 +0800

    w

--- activity8/chatroom-backend/src/users/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateUserDto {
-  @ApiProperty({ example: 'stellar_wanderer', description: 'Unique username chosen during registration' })
-  username: string;
-
-  @ApiProperty({ example: 'Str0ngP@ss!', description: 'Plain text password that will be hashed before storage' })
-  password: string;
-}
--- activity8/chatroom-backend/src/users/dto/login-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LoginUserDto {
-  @ApiProperty({ example: 'stellar_wanderer', description: 'Registered username' })
-  username: string;
-
-  @ApiProperty({ example: 'Str0ngP@ss!', description: 'Account password' })
-  password: string;
-}
